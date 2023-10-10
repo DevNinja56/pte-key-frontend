ffbfe3bcef7f6305f0fe28b91d34b678152b8320
fakharjamilsol <fakhar.jamil@solcellespesialisten.no>
2023-10-10 18:52:39 +0500

    create-some-screens

---
+++ main.js
@@ -0,0 +1,9 @@
+const ptePractice = document.querySelector(".ptePractice")
+const onShowDropdown = () => {
+    const onShowDropdown = document.querySelector(".showDropDown")
+    onShowDropdown.style.display="block"
+}
+const hideDropDown = () => {
+    const onShowDropdown = document.querySelector(".showDropDown")
+    onShowDropdown.style.display="none"
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        mainBlackColor:"#303030",
+        mainColor:"#49DEFF",
+        mainParaColor:"#4E4E4E",
+      }
+    },
+  },
+  plugins: [],
+}
+
