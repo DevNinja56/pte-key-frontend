c3b6d9e0a26bd2b9d487bd559d951fda577c35b1
fakharjamilsol <fakhar.jamil@solcellespesialisten.no>
2023-10-16 18:56:41 +0500

    complete-all-screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
         mainBlackColor:"#303030",
         mainColor:"#49DEFF",
         mainParaColor:"#4E4E4E",
-      }
+      },
+      boxShadow: {
+        inputShadow: "0px 4px 12px 0px rgba(190, 190, 190, 0.25)",
+        box_shadow: "0px 5px 25px 0px rgba(190, 190, 190, 0.25);",
+      },
     },
   },
   plugins: [],
